205812f78ce85169393aaee9a159d88ad8ae8b8d
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-21 11:46:58 +0530

    StockProblem

--- JsonInventor/Program.cs
+++ JsonInventor/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 
-namespace JsonInventor
+namespace StockProblem
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to object oriented programs Json Problem");
-            InventorDetails obj = new InventorDetails();
-
+            Console.WriteLine("This is Stock Problem");
+            Stock stock = new Stock();
+            Console.WriteLine("Give the number of stocks");
+            int NumOfStocks = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= NumOfStocks; i++)
+            {
+                Console.WriteLine("Give the sharename");
+                string ShareName = Console.ReadLine();
+                Console.WriteLine("Give the number of shares");
+                int NoOfShares = int.Parse(Console.ReadLine());
+                Console.WriteLine("Give the price per each share");
+                int PricePerEachShare = int.Parse(Console.ReadLine());
+                stock.Stockvalue(NoOfShares, PricePerEachShare, ShareName);
+            }
         }
     }
 }
